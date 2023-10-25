643ebe304cc07a700acb7aa84f9781ab59d1ee11
Ismoil <ismoilturgunpolatov1705@gmail.com>
2023-10-25 17:12:27 +0500

    vercel

--- src/postgres/postgres.js
+++ src/postgres/postgres.js
@@ -6,13 +6,17 @@ export class Postgres {
     #pg;
 
     constructor() {
+        // this.#pg = new pg.Pool({
+        //     host: process.env.DB_HOST ?? 'localhost',
+        //     port: process.env.DB_PORT ?? 5432,
+        //     user: process.env.DB_USER ?? 'postgres',
+        //     password: process.env.DB_PASSWORD ?? '#Ismoil1705',
+        //     database: process.env.DB_DATABASE ?? 'eventregulation',
+        // });
+
         this.#pg = new pg.Pool({
-            host: process.env.DB_HOST ?? 'localhost',
-            port: process.env.DB_PORT ?? 5432,
-            user: process.env.DB_USER ?? 'postgres',
-            password: process.env.DB_PASSWORD ?? '#Ismoil1705',
-            database: process.env.DB_DATABASE ?? 'eventregulation',
-        });
+            connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+        })
     }
 
     async fetch(SQL, ...params) {
