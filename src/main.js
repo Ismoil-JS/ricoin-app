b5c4eccebd7f44ca5cd89503fb96f4089449d7bd
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2023-12-06 23:34:36 +0500

    some changes to cors

--- src/main.js
+++ src/main.js
@@ -1,24 +1,33 @@
 import express from 'express';
 import cors from 'cors';
-import { config } from "dotenv";
+import { config } from 'dotenv';
 import { router } from './routes/routes.js';
 
 config();
 
 const app = express();
 
-app.use(express.json());
+// Middleware
+app.use(express.json({ limit: '100mb' })); // Increase payload size limit to 100MB
+app.use(express.urlencoded({ extended: true, limit: '10mb' })); // Set limit for url-encoded data
 app.use(cors());
 
-app.use(express.json({ limit: "100mb" }));
+// Routes
+app.use('/api', router);
 
-app.use("/api", router)
-app.use("/", (_, res) => {
-    res.send("Hello World!");
+// Default route
+app.use('/', (_, res) => {
+  res.send('Hello World!');
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Internal Server Error');
 });
 
 const port = process.env.APP_PORT || 9000;
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running at http://localhost:${port}`);
 });
--- src/module/product/product.controller.js
+++ src/module/product/product.controller.js
@@ -23,6 +23,7 @@ class ProductController {
 
     async createProduct(req, res) {
         try {
+            console.log(req.body, "conrollerga keldi");
             const product = await ProductService.createProduct(req.body);
             res.status(200).json(product);
         } catch (error) {
