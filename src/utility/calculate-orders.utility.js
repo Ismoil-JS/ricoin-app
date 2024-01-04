90f0b1d859f1def9e5eb1489c59cbc98aa0599ca
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2024-01-04 17:30:01 +0500

    coin calculator has been changed

--- src/utility/calculate-orders.utility.js
+++ src/utility/calculate-orders.utility.js
@@ -10,32 +10,40 @@ export const CalculateOrders = async (user_id, product_id, amount) => {
 
     const product = await productService.getProductById(product_id);
 
-    if(product[0].amount < amount) {return "Not enough products";}
+    if(product[0].amount < amount) {return res.status(400).send("Not enough products")}
+    else{
+        const coins = user[0].coins - (product[0].price * amount);
 
-    const coins = user[0].coins - (product[0].price * amount);
+        if(coins < 0) {return res.status(400).send("Not enough coins")}
+        else{
+            await userService.updateCoinsAndBoughtProducts({ id: user_id, coins: coins, product_id: product_id });
 
-    if(coins < 0) {return "Not enough coins";}
+            const amountOfProducts = product[0].amount - amount;
 
-    await userService.updateCoinsAndBoughtProducts({ id: user_id, coins: coins, product_id: product_id });
-
-    const amountOfProducts = product[0].amount - amount;
-
-    await productService.updateProductAmount({ id: product_id, amount: amountOfProducts });
+            await productService.updateProductAmount({ id: product_id, amount: amountOfProducts });
+        }
+    }
 }
 
 export const CancelOrders = async (order_id) => {
 
         const order = await exchangeService.getExchangeByIdNotChanged(order_id);
 
-        const user = await userService.getUserById(order[0].user_id);
+        if(order[0].status === "finished" || order.length === 0 ) {return res.status(400).send("Order is already finished or does not exist")}
+        else{
+            const user = await userService.getUserById(order[0].user_id);
 
-        const product = await productService.getProductById(order[0].product_id);
+            const product = await productService.getProductById(order[0].product_id);
         
-        const coins = user[0].coins + ((product[0].price - 2 ) * order[0].amount);
+            const coins = user[0].coins + ((product[0].price - 2 ) * order[0].amount);
 
-        await userService.updateCoins({ id: user[0].id, coins: coins });
+            await userService.updateCoins({ id: user[0].id, coins: coins });
 
-        const amountOfProducts = product[0].amount + order[0].amount;
+            const amountOfProducts = product[0].amount + order[0].amount;
 
-        await productService.updateProductAmount({ id: product[0].id, amount: amountOfProducts });
+            await productService.updateProductAmount({ id: product[0].id, amount: amountOfProducts });
+
+            await exchangeService.cancelExchange(order_id);
+        }
+        
 }
