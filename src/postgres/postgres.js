46c20abe37454bd346f96bd2d68dac4bd89e2b97
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2025-02-24 15:25:28 +0500

    new database

--- src/postgres/postgres.js
+++ src/postgres/postgres.js
@@ -3,27 +3,27 @@ import { config } from "dotenv";
 config();
 
 export class Postgres {
-    #pg;
+  #pg;
 
-    constructor() {
-        this.#pg = new pg.Pool({
-            host: process.env.DB_HOST ?? 'localhost',
-            port: process.env.DB_PORT ?? 5432,
-            user: process.env.DB_USER ?? 'postgres',
-            password: process.env.DB_PASSWORD ?? '1234',
-            database: process.env.DB_DATABASE ?? 'ricoin',
-        });
-    }
+  constructor() {
+    this.#pg = new pg.Pool({
+      host: process.env.DB_HOST ?? "localhost",
+      port: process.env.DB_PORT ?? 5432,
+      user: process.env.DB_USER ?? "postgres",
+      password: process.env.DB_PASSWORD ?? "0202",
+      database: process.env.DB_DATABASE ?? "eventregulation",
+    });
+  }
 
-    async fetch(SQL, ...params) {
-        const client = await this.#pg.connect();
-        try {
-            const { rows } = await client.query(SQL, params.length ? params : null);
-            return rows;
-        } catch (e) {
-            console.log(e);
-        } finally {
-            client.release();
-        }
+  async fetch(SQL, ...params) {
+    const client = await this.#pg.connect();
+    try {
+      const { rows } = await client.query(SQL, params.length ? params : null);
+      return rows;
+    } catch (e) {
+      console.log(e);
+    } finally {
+      client.release();
     }
+  }
 }
