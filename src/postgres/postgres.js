96e9a022dd3b17d00bdb3b31f7d7919de9f87551
Ismoil-JS <ismoilturgunpolatov1705@gmail.com>
2025-02-24 15:26:03 +0500

    new database

--- src/postgres/postgres.js
+++ src/postgres/postgres.js
@@ -10,7 +10,7 @@ export class Postgres {
       host: process.env.DB_HOST ?? "localhost",
       port: process.env.DB_PORT ?? 5432,
       user: process.env.DB_USER ?? "postgres",
-      password: process.env.DB_PASSWORD ?? "0202",
+      password: process.env.DB_PASSWORD ?? "1416",
       database: process.env.DB_DATABASE ?? "eventregulation",
     });
   }
